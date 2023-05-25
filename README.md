22ab661adf487899c58ddab4ad6552a0870a0f8a
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2023-05-25 21:29:44 +0900

    :construction: WIP : each freq

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Capacitor parallel RLC Fitting
+
+## Equivarent Circuit
+
+
+## how to fit
+In each frequency, the cost function is 
+$$ f_{cost} = \Sigma_{i,j} \left( s_{i,j}^{ref} - s_{i,j}^{lumped} \right) $$
+
+$s^{ref}, s^{lumped}$ is reference data obtained from `simsurfing` and calculated data from parallel RLC circuit. 
--- scripts/fitting.py
+++ scripts/fitting.py
@@ -4,12 +4,8 @@ from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
 
-def parallel_RLC(freq, param : np.ndarray):
-    p = np.split(param, 3)
-
-    R = p[0]
-    L = p[1]
-    C = p[2]
+def parallel_RLC(f, R, L, C):
+    freq = rf.Frequency(f,f,npoints=1,unit='hz')
 
     line = rf.media.DefinedGammaZ0(frequency=freq, z0=50)
     res = line.resistor(R, name='R')
@@ -28,29 +24,32 @@ def parallel_RLC(freq, param : np.ndarray):
     ntw = cir.network
     ntw.name = 'lumped circuit'
 
-    return ntw.s
+    return ntw.s.reshape(4)
 
 
 if __name__ == '__main__':
     simsurfing = rf.Network('data/GJM1555C1HR50BB01_DC0V_25degC_series.s2p')
     simsurfing.frequency.unit = 'ghz'
 
-    freq = simsurfing.frequency
+    freq = simsurfing.frequency.f
 
-    R = np.full(freq.f.size, 1e4)
-    L = np.full(freq.f.size, 1e-12)
-    C = np.full(freq.f.size, 1e-9)
+    R = 1e4
+    L = 1e-12
+    C = 1e-9
 
-    param = np.concatenate([R, L, C], axis=0)
-    s = parallel_RLC(freq, param)
+    # for f in freq:
+    f = freq[0]
 
-    simsurfing.plot_s_db(n=0, m=0, c='r' ,linestyle='dashed')
-    simsurfing.plot_s_db(n=0, m=1, c='b' ,linestyle='dashed')
+    s = parallel_RLC(f, R, L, C)
+    print(s.shape)
 
-    plt.plot(freq.f, 10*np.log10(s[:,0,0].real), c='r', label='opt, S11')
-    plt.plot(freq.f, 10*np.log10(s[:,1,0].real), c='b', label='opt, S21')
+    # simsurfing.plot_s_db(n=0, m=0, c='r' ,linestyle='dashed')
+    # simsurfing.plot_s_db(n=0, m=1, c='b' ,linestyle='dashed')
 
-    plt.xlim(1e9, 20e9)
-    plt.ylim(-30, 0)
-    plt.legend()
-    plt.savefig('a.png')
+    # plt.plot(freq.f, 10*np.log10(s[:,0,0].real), c='r', label='opt, S11')
+    # plt.plot(freq.f, 10*np.log10(s[:,1,0].real), c='b', label='opt, S21')
+
+    # plt.xlim(1e9, 20e9)
+    # plt.ylim(-30, 0)
+    # plt.legend()
+    # plt.savefig('a.png')
